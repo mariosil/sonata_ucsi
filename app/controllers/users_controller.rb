e1f1c30107433c44fa685abdb5a6d149d39ceade
mariosil <silveira.mario.lab@gmail.com>
2021-04-09 06:36:05 -0500

    UCSI-3 Setting up rolify

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,40 +23,34 @@ class UsersController < ApplicationController
   def edit
   end
 
-  # POST /users or /users.json
   def create
     @user = User.new(user_params)
+    @user.add_role :admin if will_be_admin
 
     respond_to do |format|
       if @user.save
         format.html { redirect_to :users, notice: 'User was created.' }
-        format.json { render :show, status: :created, location: @user }
       else
         format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PATCH/PUT /users/1 or /users/1.json
   def update
     respond_to do |format|
       if @user.update(user_params)
+        will_be_admin ? @user.add_role(:admin) : @user.remove_role(:admin)
         format.html { redirect_to @user, notice: 'User was updated.' }
-        format.json { render :show, status: :ok, location: @user }
       else
         format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /users/1 or /users/1.json
   def destroy
     @user.destroy
     respond_to do |format|
       format.html { redirect_to users_url, notice: 'User was destroyed.' }
-      format.json { head :no_content }
     end
   end
 
@@ -69,6 +63,15 @@ class UsersController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation)
+    user_params = params.require(:user).permit :email,
+                                                 :password,
+                                                 :password_confirmation
+    user_params.delete(:password) if user_params[:password].blank?
+    user_params.delete(:password_confirmation) if user_params[:password_confirmation].blank?
+    user_params
+  end
+
+  def will_be_admin
+    @will_be_admin ||= params.require(:user).permit(:is_admin) == '1'
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,11 @@
 
 # Maps user table
 class User < ApplicationRecord
+  rolify
   authenticates_with_sorcery!
 
   validates :email, uniqueness: true
-  validates :password, length: { minimum: 3 }
-  validates :password, confirmation: true
-  validates :password_confirmation, presence: true
+  validates :password, length: { minimum: 6 }, if: :new_record?
+  validates :password, confirmation: true, if: :new_record?
+  validates :password_confirmation, presence: true, if: :new_record?
 end
